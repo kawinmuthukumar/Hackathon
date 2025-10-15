99f7a2aaca4a2728d81aa3389222c30e2c4c42fa
kawin <kawin>
2025-10-16 00:24:26 +0530

    Modified files 2.0

--- backend.tf
+++
@@ -1,8 +0,0 @@
-# terraform/backend.tf
-terraform {
-  backend "s3" {
-    bucket = "spacelift-terraform-state-382345110959"
-    key    = "hackathon/terraform.tfstate"
-    region = "us-west-2"
-  }
-}
